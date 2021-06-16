5ebc48b77e0d2e4bf6a0b2fbcdaa93c13f600105
Kyle Arrowood <kyle.arrowood@xylem.com>
2021-06-16 14:22:43 -0400

    Initial commit in the branch for the new API. Took the source code needed to compile and build the epanetmsx.dll and put that into the Legacy DLL folder, that folder also contains the EPANET repository as of right now. Took the source code required to compile and create the CLI executable and put it into the CLI folder.

---
+++ Legacy DLL/CompileNotes.md
@@ -0,0 +1,12 @@
+Compiling with CMake
+
+
+Compiling
+1. Create a directory named "build" inside the root directory.
+2. Navigate into the directory.
+3. Run `cmake ..`
+4. Run `cmake --build . --config Release`
+
+
+The epanetmsx.dll will be built in the bin/Release directory.
+
--- build/Xcode/epanetmsx/build/epanetmsx.build/Debug/epanetmsx.build/Objects-normal/x86_64/msxfuncs.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/sam/Documents/Software/Code_Repositories/EPANET_SourceForge/MSX/trunk/build/Xcode/epanetmsx/../../../src/msxfuncs.c \
-  /Users/sam/Documents/Software/Code_Repositories/EPANET_SourceForge/MSX/trunk/build/Xcode/epanetmsx/../../../src/msxfuncs.h
